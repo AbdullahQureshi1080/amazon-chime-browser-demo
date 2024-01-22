f1454b94e5eaeae9994fa0145d756047701bdc38
Abdullah Qureshi <adnx1080@gmail.com>
2024-01-22 13:13:07 +0500

    feat: demo configured & tested locally

---
+++ build/demos/browser/app/meetingV2/video/VideoPreferenceManager.d.ts
@@ -0,0 +1,17 @@
+import { Logger, TargetDisplaySize, VideoPreference, VideoQualityAdaptationPreference, VideoPriorityBasedPolicy, VideoSource } from 'amazon-chime-sdk-js';
+export default class VideoPreferenceManager {
+    private logger;
+    private downlinkPolicy;
+    static readonly DefaultVideoTilePriority: number;
+    static readonly DefaultVideoTileTargetDisplaySize: TargetDisplaySize;
+    attendeeIdToVideoPreference: Map<string, VideoPreference>;
+    priorityBasedDownlinkPolicy: VideoPriorityBasedPolicy | null;
+    _visibleAttendees: string[];
+    set visibleAttendees(value: Array<string>);
+    constructor(logger: Logger, downlinkPolicy: VideoPriorityBasedPolicy);
+    ensureDefaultPreferences(videoSources: VideoSource[]): void;
+    setAttendeeTargetDisplaySize(attendeeId: string, targetDisplaySize: TargetDisplaySize): void;
+    setAttendeePriority(attendeeId: string, priority: number): void;
+    setAttendeeDegradationPreference(attendeeId: string, preference: VideoQualityAdaptationPreference): void;
+    private updateDownlinkPreference;
+}
